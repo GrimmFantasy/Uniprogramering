94f5770a602e0ea0f98a334e8dc721dbcda43f70
GrimmFantasy <capser11andersen@hotmail.com>
2020-10-14 12:58:47 +0200

    opgave 5.3 done

---
+++ Goldbachs Formodning.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include "D:\coolcode\Lib\isPrime.h"
+
+int main ()
+{
+
+    int i, f, evennr, g, y = 1;
+    int Primtal[200000];
+    Primtal[0] = 0;
+
+    for (i=1; i<=200000; i+=2)
+    {
+        if (is_prime(i))
+        {
+            Primtal[y] = i;
+            y++;
+        }
+    }
+    printf("%d \n", Primtal[10]);
+    y = 1;
+    for (evennr = 6; evennr <= 200000; evennr+=2)
+    {
+        while(Primtal[y] < evennr)
+        {
+            g = evennr - Primtal[y];
+            if (is_prime(g))
+            {
+                printf("%d is works cause %d and %d is a uneven primes\n", evennr, Primtal[y], g);
+            }
+            y++;
+        }
+        y=1;
+
+    }
+
+    return 0;
+}
+
---
+++ Lib/isPrime.h
@@ -0,0 +1,18 @@
+int is_prime(int i){
+   assert(i >= 0);
+
+   if (i == 1)
+     return 0;
+   else if (i == 2)
+     return 1;
+   else if (i % 2 == 0)
+     return 0;
+   else{
+     int k, limit;
+     limit = (int)(ceil(sqrt(i)));
+     for (k = 3; k <= limit; k += 2)
+        if (i % k == 0)
+           return 0;
+     return 1;
+   }
+}
